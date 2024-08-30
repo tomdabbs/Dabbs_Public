7294d6d53db1bad57b6bdc0583dd91ec3a47e729
tomdabbs <tom.dabbs@sollus.us>
2024-08-30 06:26:01 -0400

    Create PuLP_Optimization.py

---
+++ Python/Optimization/PuLP_Optimization.py
@@ -0,0 +1,69 @@
+from pulp import *
+import datetime
+import pandas as pd
+
+costs = r'C:\Users\Owner\OneDrive - SOLLUS\SOLLUS\Python\Optimization\MiniOpt\zCosts.xlsx'
+constraints = r'C:\Users\Owner\OneDrive - SOLLUS\SOLLUS\Python\Optimization\MiniOpt\zConstraints.xlsx'
+demand = r'C:\Users\Owner\OneDrive - SOLLUS\SOLLUS\Python\Optimization\MiniOpt\zDemand.xlsx'
+variable=r'C:\Users\Owner\OneDrive - SOLLUS\SOLLUS\Python\Optimization\MiniOpt\variable.txt'
+problem=r'C:\Users\Owner\OneDrive - SOLLUS\SOLLUS\Python\Optimization\MiniOpt\problem.txt'
+solution = r'C:\Users\Owner\OneDrive - SOLLUS\SOLLUS\Python\Optimization\MiniOpt\DToutput.txt'
+
+#Facility Information
+facility=pd.read_excel(constraints, sheet_name='Constraints')
+facility=pd.DataFrame(facility,columns=['Facility','Product','MaxCycles','MinCycles'])
+facility_l=[]
+[facility_l.append(x) for x in facility['Facility'] if x not in facility_l]
+#Store Information
+stores=pd.read_excel(demand, sheet_name='Demand')
+stores=pd.DataFrame(stores,columns=['Store','Product','Demand'])
+stores_l=[]
+[stores_l.append(x) for x in stores['Store'] if x not in stores_l]
+#Product Information
+product_l=['DT','FD']
+#Costs Information
+costs=pd.read_excel(costs, sheet_name='Costs')
+costs=pd.DataFrame(costs,columns=['Facility','Store','Product','TotalCost'])
+costs=costs.set_index(['Facility','Store','Product'])['TotalCost'].to_dict()
+#Constraint Information - Dictionary
+DC_max=facility.set_index(['Facility','Product'])['MaxCycles'].to_dict()
+DC_min=facility.set_index(['Facility','Product'])['MinCycles'].to_dict()
+demand=stores.set_index(['Store','Product'])['Demand'].to_dict()
+
+#####_____PULP_OPTIMIZATION_____####
+
+#State LP Problem
+prob = LpProblem("DToptimization",LpMinimize)
+
+#Variables
+vars = LpVariable.dict('flow',(facility_l,stores_l,product_l),0,None,LpContinuous) 
+
+#Objective Function
+print('Construct_OBjective_Function')
+print("current time:-", datetime.datetime.now())
+prob += lpSum([vars[f]*costs[f] for f in costs])
+
+#Constraints
+print('Construct_Constraints_Demand')
+print("current time:-", datetime.datetime.now())
+#for i in facility_l:
+for j in stores_l:
+    for k in product_l:
+        prob += lpSum(vars[i,j,k] for i in facility_l)==demand[j,k]
+print('Construct_Constraints_MaxCycles')
+print("current time:-", datetime.datetime.now())
+for i in facility_l:
+    #for j in stores_l:
+        for k in product_l:
+            prob += lpSum(vars[i,j,k] for j in stores_l)<=DC_max[i,k]
+
+#Solution
+prob.solve() # This solves the linear problem
+prob.writeLP(solution) # The status of the solution is printed to the screen
+
+#Outputs
+print("Total Cost = ", value(prob.objective))
+print('Write to File')
+print("current time:-", datetime.datetime.now())
+print('Finished')
+print("current time:-", datetime.datetime.now())
