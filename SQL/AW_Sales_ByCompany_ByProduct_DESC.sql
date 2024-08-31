a0bbd711f418f2bcaf6509e225644019fd47e9c9
tomdabbs <tom.dabbs@sollus.us>
2024-08-31 17:07:41 -0400

    Create AW_Sales_ByCompany_ByProduct_DESC.sql

---
+++ SQL/AW_Sales_ByCompany_ByProduct_DESC.sql
@@ -0,0 +1,18 @@
+
+USE AdventureWorksLT2012
+
+SELECT SalesLT.Customer.CompanyName, SalesLT.Product.Name, Sum(SalesLT.SalesOrderDetail.LineTotal) AS SumOfLineTotal
+FROM SalesLT.Product INNER JOIN 
+(SalesLT.Customer INNER JOIN 
+(SalesLT.SalesOrderHeader INNER JOIN SalesLT.SalesOrderDetail ON SalesLT.SalesOrderHeader.SalesOrderID = SalesLT.SalesOrderDetail.SalesOrderID)
+ON SalesLT.Customer.CustomerID = SalesLT.SalesOrderHeader.CustomerID) 
+ON SalesLT.Product.ProductID = SalesLT.SalesOrderDetail.ProductID
+GROUP BY SalesLT.Customer.CompanyName, SalesLT.Product.Name
+ORDER BY Sum(SalesLT.SalesOrderDetail.LineTotal) DESC;
+
+
+
+
+
+
+
