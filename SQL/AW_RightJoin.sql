0d968f729ed7aff8ab391bb39f84571e927aa089
tomdabbs <tom.dabbs@sollus.us>
2024-08-31 17:07:34 -0400

    Delete AW_RightJoin.sql

--- SQL/AW_RightJoin.sql
+++
@@ -1,6 +0,0 @@
-USE AdventureWorksLT2012
-
-SELECT SalesLT.Product.Name, Count(SalesLT.SalesOrderDetail.SalesOrderID) AS SalesOrderCount
-FROM SalesLT.SalesOrderDetail RIGHT JOIN SalesLT.Product ON SalesLT.SalesOrderDetail.ProductID = SalesLT.Product.ProductID
-GROUP BY SalesLT.Product.Name
-ORDER BY SalesLT.Product.Name;
