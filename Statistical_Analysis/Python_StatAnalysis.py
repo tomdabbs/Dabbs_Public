dc53ddf2314a64100dd19824cf3081a4f8277e5c
tomdabbs <tom.dabbs@sollus.us>
2024-09-07 06:38:26 -0400

    Update Python_StatAnalysis.py

--- Statistical_Analysis/Python_StatAnalysis.py
+++ Statistical_Analysis/Python_StatAnalysis.py
@@ -1,21 +1,12 @@
 
 import pandas as pd
-import matplotlib
 import matplotlib.pyplot as plt
 
 input = r'C:\Users\Owner\OneDrive - SOLLUS\Personal\Documents\GitHub\Dabbs_Public\Statistical_Analysis\StoreDemand.xlsx'
 
 storeinfo = pd.read_excel(input, sheet_name='StoreDemand')
-
 dfstats = storeinfo.groupby(['State','Product'])['StoreDemand'].describe()
 print(dfstats)
-
-# Histogram = State and Product specific
-storeinfo['StoreDemand'].plot(kind='hist', bins=10, edgecolor='black')
-plt.title('Histogram Plot')
-plt.xlabel('Values')
-plt.ylabel('Frequency')
-plt.show()
 
 # Histogram = State and Product specific
 state = "AL"
@@ -25,7 +16,7 @@ df.plot(kind='hist', y='StoreDemand', bins=10, edgecolor='black')
 plt.title('Histogram of ' + state + ' - ' + product)
 plt.show()
 
-
+# Pie Chart = 
 
 
 
