b1826611f29d04b6126c9634f128eb5fa893cb89
tomdabbs <tom.dabbs@sollus.us>
2024-09-10 13:50:33 -0400

    Create AW_UnitsSolds_ByProduct_ASC.sql

---
+++ PLSQL/AW_UnitsSolds_ByProduct_ASC.sql
@@ -0,0 +1,12 @@
+
+SELECT 
+    p.Name AS ProductName, 
+    COUNT(sod.SalesOrderID) AS SalesOrderCount
+FROM 
+    Product p
+LEFT JOIN 
+    SalesOrderDetail sod ON p.ProductID = sod.ProductID
+GROUP BY 
+    p.Name
+ORDER BY 
+    p.Name;
