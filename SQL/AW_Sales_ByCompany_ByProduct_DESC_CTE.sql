2c1bfde56fa1731abaa6f146ab0b489bfc1db3f0
tomdabbs <tom.dabbs@sollus.us>
2024-09-13 13:27:09 -0400

    Create AW_Sales_ByCompany_ByProduct_DESC_CTE.sql

---
+++ SQL/AW_Sales_ByCompany_ByProduct_DESC_CTE.sql
@@ -0,0 +1,28 @@
+
+USE AdventureWorksLT2012
+
+;WITH SalesData AS (
+    SELECT 
+        c.CompanyName, 
+        p.Name, 
+        sod.LineTotal
+    FROM 
+        SalesLT.Product AS p
+    INNER JOIN SalesLT.SalesOrderDetail AS sod 
+        ON p.ProductID = sod.ProductID
+    INNER JOIN SalesLT.SalesOrderHeader AS soh 
+        ON sod.SalesOrderID = soh.SalesOrderID
+    INNER JOIN SalesLT.Customer AS c 
+        ON soh.CustomerID = c.CustomerID
+)
+SELECT 
+    CompanyName, 
+    Name, 
+    SUM(LineTotal) AS SumOfLineTotal
+FROM 
+    SalesData
+GROUP BY 
+    CompanyName, 
+    Name
+ORDER BY 
+    SumOfLineTotal DESC;
